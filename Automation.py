b5ef59875efa5f179396daf2d6867091320e11c7
rafael <test@test.com>
2024-03-12 16:26:45 -0300

    none

--- Automation.py
+++ Automation.py
@@ -2,7 +2,7 @@
 Python exercise from "Python Journey" Python training by Hashtag.
 
 Challenge: given a file ("produtos.csv") containing various information about hypothetical products,
-such as code, brand, type, category, price, note, etc, create an automation program to read and extract
+such as code, brand, type, category, price, note, etc., create an automation program to read and extract
 the information on that file and register them in the e-commerce mock website.
 
 Important: the program must use the following libraries - PyAutoGUI; pandas.
